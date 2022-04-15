e89ef2a7f53520e97b97b6d7000a87f345ef3b72
Silderion <mpatau548@gmail.com>
2022-04-15 14:06:25 +0200

    LogInUpdate 1

--- src/app/composants/login/login.component.ts
+++ src/app/composants/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = "Mark";
+  password = "1234";
+  message = false;
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
-  login(login:any){
-
+  login(loginForm:any){
+    if((loginForm.value.username == this.username) && (loginForm.value.password == this.password)){
+      this.router.navigate(["product"])
+    } else {
+      this.message = true;
+    }
   }
 
 }
